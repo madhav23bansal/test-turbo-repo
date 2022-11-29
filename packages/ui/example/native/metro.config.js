7d0a8176370846b4dbc036b4a69ba9b4a73b0672
madhav23bansal <madhavb230100@gmail.com>
2022-11-29 16:17:38 +0530

    fixed for ios

--- packages/ui/example/native/metro.config.js
+++ packages/ui/example/native/metro.config.js
@@ -1,9 +1,12 @@
 // Learn more https://docs.expo.io/guides/customizing-metro
 const { getDefaultConfig } = require("expo/metro-config");
 const path = require("path");
+const findWorkspaceRoot = require("find-yarn-workspace-root");
 
 // Find the workspace root, this can be replaced with `find-yarn-workspace-root`
-const workspaceRoot = path.resolve(__dirname, "../../");
+// const workspaceRoot = path.resolve(__dirname, "../../");
+
+const workspaceRoot = findWorkspaceRoot(__dirname);
 const projectRoot = __dirname;
 
 const config = getDefaultConfig(projectRoot);
